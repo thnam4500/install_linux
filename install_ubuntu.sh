f496b483f103e6bd2f9b4c17c396ad48b79a7f2a
Hoang Nam <thnam4500@gmail.com>
2022-01-04 05:41:22 +0700

    Add ubuntu_install and change name install_file into install_opensuse

---
+++ install_ubuntu.sh
@@ -0,0 +1,6 @@
+sudo apt install -y git nodejs neovim
+cd ./.config/;git clone https://github.com/thnam4500/nvim.git;cd
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt update
+sudo apt install apt-fast
+sudo apt-fast upgrade
